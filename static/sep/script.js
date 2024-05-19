d97c0708fb4f28e94f64bb4236f9411aad67993a
Kakushigoto <nikita.zaika.2000@gmail.com>
2024-05-19 12:16:16 +0300

    mobile support

--- static/sep/script.js
+++ static/sep/script.js
@@ -7,7 +7,18 @@ window.addEventListener('scroll', function() {
     const scrollToTop = document.querySelector('.fixed-button');
     const heroSection = document.getElementById('hero');
     const heroSectionHeight = heroSection.offsetHeight;
+    const secondSection = document.getElementById('features'); // замените 'features' на id второго блока
+    const secondSectionHeight = secondSection.offsetHeight;
 
+    // Определяем, насколько пользователь прокрутил страницу
+    const scrollProgress = Math.min(window.scrollY / secondSectionHeight, 1);
+
+    // Устанавливаем прозрачность фона navbar
+    const maxOpacity = 0.95; // Максимальная непрозрачность
+    const backgroundColor = `rgba(0, 0, 0, ${scrollProgress * maxOpacity})`;
+    navbar.style.backgroundColor = backgroundColor;
+
+    // Изменяем фон navbar по мере прокрутки
     if (window.scrollY > heroSectionHeight) {
         navbar.classList.add('scrolled');
         scrollToTop.classList.add('scrolled');
@@ -16,7 +27,6 @@ window.addEventListener('scroll', function() {
         scrollToTop.classList.remove('scrolled');
     }
 });
-
 
 // Ваша функция переключения языка
 function switchLanguage(languageCode) {
@@ -43,6 +53,9 @@ function switchLanguage(languageCode) {
 document.getElementById('lang-en').addEventListener('click', () => switchLanguage('en'));
 document.getElementById('lang-ru').addEventListener('click', () => switchLanguage('ru'));
 document.getElementById('lang-uk').addEventListener('click', () => switchLanguage('uk'));
+document.getElementById('lang-mob-en').addEventListener('click', () => switchLanguage('en'));
+document.getElementById('lang-mob-ru').addEventListener('click', () => switchLanguage('ru'));
+document.getElementById('lang-mob-uk').addEventListener('click', () => switchLanguage('uk'));
 
 // По умолчанию загружаем английский язык
 switchLanguage('en');
@@ -61,5 +74,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
                 behavior: 'smooth' // Используем плавную прокрутку
             });
         }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerMenu.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active');
     });
 });
